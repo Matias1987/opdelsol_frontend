3e42e0a1732c532317af71dbf0fc9ea37729c4df
Bruno <ch.matias87@gmail.com>
2023-05-05 19:06:33 -0300

    varios

--- pages/v1/deposito/index.jsx
+++ pages/v1/deposito/index.jsx
@@ -8,15 +8,15 @@ export default function Index(){
         
         <Divider />
         <br />
-        <Button style={{width:"130px", height:"130px", wordBreak: "break-all", margin: "1em", color:"red", borderColor:"black"}} onClick={()=>{window.location.replace(public_urls.nuevo_envio)}}>
+        <Button style={{width:"130px", height:"130px", wordBreak: "break-all", margin: "1em", color:"red", borderColor:"black"}} onClick={()=>{window.location.href = public_urls.nuevo_envio}}>
             <GiftOutlined /><br />
             <b>Nuevo Envio</b>
         </Button>
-        <Button style={{width:"130px", height:"130px", wordBreak: "break-all", margin: "1em", color:"orange", borderColor:"black"}} onClick={()=>{window.location.replace(public_urls.lista_stock)}}>
+        <Button style={{width:"130px", height:"130px", wordBreak: "break-all", margin: "1em", color:"orange", borderColor:"black"}} onClick={()=>{window.location.href = public_urls.lista_stock}}>
             <BlockOutlined /><br />
             <b>Stock</b>
         </Button>
-        <Button style={{width:"130px", height:"130px", wordBreak: "break-all", margin: "1em", color:"green", borderColor:"black"}} onClick={()=>{window.location.replace(public_urls.editar_multiplicadores)}}>
+        <Button style={{width:"130px", height:"130px", wordBreak: "break-all", margin: "1em", color:"green", borderColor:"black"}} onClick={()=>{window.location.href = public_urls.editar_multiplicadores}}>
             <DollarOutlined /><br /> 
             <b>Cambiar Precios</b>
         </Button>
