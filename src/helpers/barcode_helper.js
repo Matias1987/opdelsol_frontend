af5713c115a3a9c2630c863a8a31a1c6ca92467d
Bruno <ch.matias87@gmail.com>
2023-06-14 20:28:01 -0300

    varios

--- src/helpers/barcode_helper.js
+++ src/helpers/barcode_helper.js
@@ -1,4 +1,4 @@
-const regex_format = /^SL[0-9]+$/
+const regex_format = /^__SL[0-9]+$/
 
 const exec_barcode_pattern = (str) => regex_format.exec(str)
 
@@ -9,10 +9,10 @@ const get_barcode_from_id = (str) => {
 	for(let i=0;i<6;i++){
 		r = (i < str.length ? str.charAt(str.length -1 -i) : "0") + r ;
 	}
-	return "SL"+r;
+	return "__SL"+r;
 }
 const get_barcode_from_id2 = (id) => {
-	return "SL"+id;
+	return "__SL"+id;
 }
 
 const regex_get_id_if_match = (str) => {
