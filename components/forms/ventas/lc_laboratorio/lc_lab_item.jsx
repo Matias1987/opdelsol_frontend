08db27d7443a7d02eb6b4ba4bf9a6e2f0ae0db7c
Bruno <ch.matias87@gmail.com>
2023-10-28 12:57:11 -0300

    varios

--- components/forms/ventas/lc_laboratorio/lc_lab_item.jsx
+++ components/forms/ventas/lc_laboratorio/lc_lab_item.jsx
@@ -11,11 +11,21 @@ const LCLabItem = (props) =>{
         codigo: null,
         precio: 0,
         cantidad:1,
+        cb: 0,
+        diametro: 0,
     })
     const on_codigo_change = (val) => {
         
         setLC((LC)=>{
             const __LC ={...LC,codigo:val.codigo, precio: val.precio, idcodigo: val.idcodigo};
+            props?.callback?.(__LC);
+            return __LC;
+        })
+    }
+
+    const on_change = (val, idx) => {
+        setLC((LC)=>{
+            const __LC ={...LC,[idx]:val};
             props?.callback?.(__LC);
             return __LC;
         })
@@ -45,10 +55,16 @@ const LCLabItem = (props) =>{
             }</Button>  :
         <>
             <Row>
-                <Col span={17}>
+                <Col span={11}>
                     <SelectCodigoVenta idfamilias={[globals.familiaIDs.LC]} callback={on_codigo_change} />
                 </Col>
-                <Col span={6}>
+                <Col span={4}>
+                    <Input size="small" prefix="CB" type="number" value={LC.cb} onChange={(e=>{on_change(e.target.value,"cb")})} />
+                </Col>
+                <Col span={4}>
+                    <Input size="small" prefix="Diámetro" type="number" value={LC.diametro} onChange={(e=>{on_change(e.target.value,"diametro")})} />
+                </Col>
+                <Col span={4}>
                     <Input size="small" style={{backgroundColor:"rgba(131,137,150, 0.4)"}} readOnly type="number" prefix={"Precio: "} value={LC.precio} onChange={(v)=>{on_precio_change(v)}} />
                 </Col>
                 <Col span={1}>
