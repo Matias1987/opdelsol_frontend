af776270ea76b320824d6a99284aeacd2e543f84
matias <devoptdelsol@gmail.com>
2023-12-06 08:46:02 -0300

    varios

--- components/forms/caja/PopupAutorizacionesPendientes.jsx
+++ components/forms/caja/PopupAutorizacionesPendientes.jsx
@@ -1,5 +1,33 @@
+import { Col, Row, Table } from "antd"
+import { useState } from "react"
+
 const PopupNotificacionesPendientes = () => {
-    return <></>
+    const [dataSource, setDataSource] = useState([])
+    const columns = [
+        {dataIndex: "idusuario", title:"idusuario"},
+        {dataIndex: "nombre", title: "Nombre"},
+        {dataIndex: "estado", title: "Estado"},
+        {dataIndex: "idusuario", title:"Acciones", render:(_,{idusuario})=>(
+            <>
+
+            </>
+        )}
+
+    ]
+    return <>
+    <Row>
+        <Col span={24}>
+            <h4>Autorizaciones Pendientes</h4>
+        </Col>
+    </Row>
+    <Row>
+        <Col span={24}>
+            <Table dataSource={dataSource} columns={columns} />
+        </Col>
+    </Row>
+    
+    
+    </>
 }
 
 export default PopupNotificacionesPendientes
