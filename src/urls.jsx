1503cf42a7d3c2d0980314127ac235f75a79d2e9
bruno <ch.matias87@gmail.com>
2023-12-12 16:45:32 -0300

    varios

--- src/urls.jsx
+++ src/urls.jsx
@@ -1,6 +1,6 @@
 const remote_base_url = "http://54.174.39.15:3001/api/v1/"
-const local_base_url = "http://gestion.opticadelsol.com/v1/"
-//const local_base_url = "http://54.90.66.197:3000/v1/"
+//const local_base_url = "http://gestion.opticadelsol.com/v1/"
+const local_base_url = "http://54.174.39.15:3000/v1/"
 
 //const remote_base_url = "http://localhost:3001/api/v1/"
 //const local_base_url = "http://localhost:3000/v1/"
