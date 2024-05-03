4bcef61ac678b7ab0c29fa503a39c0625b88c15e
Bruno <ch.matias87@gmail.com>
2024-05-03 12:57:55 -0300

    varios

--- components/taller/pedidos.jsx
+++ components/taller/pedidos.jsx
@@ -1,5 +1,17 @@
+import { Table } from "antd";
+import { useState } from "react";
+
 const ListaPedidos = () => {
-    return <></>
+    const [pedidos, setPedidos] = useState([])
+    const columns = [
+        {dataIndex:"codigo",title:"Codigo"},
+        {render:(_,obj)=>{
+            return <></>
+        }}
+    ]
+    return <>
+        <Table />
+    </>
 }
 
 export default ListaPedidos;
--- pages/v1/laboratorio/lista_operaciones_laboratorio.jsx
+++ pages/v1/laboratorio/lista_operaciones_laboratorio.jsx
@@ -1,17 +1,59 @@
 import LayoutLaboratorio from "@/components/layout/layout_laboratorio";
 import EditarSobre from "@/components/taller/EditarSobre";
-import { Modal } from "antd";
+import { SearchOutlined } from "@ant-design/icons";
+import { Button, Col, Input, Modal, Row } from "antd";
 import { useEffect, useState } from "react";
 
 const { default: ListaVentas } = require("@/components/informes/ventas/ListaVentas")
 
 export default function  ListaOperacionesLab(){
     const [idventa, setIdVenta] = useState(-1)
+    const [busqueda, setBusqueda] = useState("")
+    const [idBusqueda, setIdBusqueda] = useState(-1)
     const [open, setOpen] = useState(false)
     const [reload, setReload] = useState(false)
     useEffect(()=>{},[])
     return <>
+    <Row>
+        <Col span={24}>
+            <Input 
+            allowClear 
+            style={{width:"100%"}} 
+            prefix="Buscar por Nro. de Venta" 
+            addonAfter={<><Button 
+                onClick={()=>
+                    {
+                        setIdBusqueda((busqueda||"").length<1 ? "-1" : busqueda);
+                        setReload(!reload)
+                    }}><SearchOutlined /></Button></>} 
+            onChange={(e)=>{
+                setBusqueda(e.target.value)
+            }} 
+            value={busqueda} />
+        </Col>
+    </Row>
+    <Row>
+        <Col span={24}>
+            {
+                idBusqueda>0?
+        <ListaVentas id={idBusqueda} marcarTerminado ignoreSucursal laboratorio_modificar enviar_a_sucursal en_laboratorio={1} ignoreSucursalEntrega  estado={"PENDIENTE"} onEditLaboratorioClick={(id)=>{setIdVenta(id), setOpen(true)}} key={reload} />
+        :
         <ListaVentas marcarTerminado ignoreSucursal laboratorio_modificar enviar_a_sucursal en_laboratorio={1} ignoreSucursalEntrega  estado={"PENDIENTE"} onEditLaboratorioClick={(id)=>{setIdVenta(id), setOpen(true)}} key={reload} />
+        
+            }
+        
+        </Col>
+    </Row>
+    <Row>
+        <Col span={24}>
+
+        </Col>
+    </Row>
+    <Row>
+        <Col span={24}>
+
+        </Col>
+    </Row>
         <Modal open={open} footer={null} onCancel={()=>{setOpen(false)}} key={idventa} width={"80%"}>
             <EditarSobre idventa={idventa} callback={()=>{setReload(!reload), setOpen(false)}} />
         </Modal>
