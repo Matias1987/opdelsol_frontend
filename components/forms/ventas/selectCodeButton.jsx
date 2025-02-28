325960c696ec71bde45b0a7991901c1993f018e0
bruno <ch.matias87@gmail.com>
2025-02-28 17:56:49 -0300

    varios

--- components/forms/ventas/selectCodeButton.jsx
+++ components/forms/ventas/selectCodeButton.jsx
@@ -1,5 +1,7 @@
+import { EditOutlined } from "@ant-design/icons"
+
 const SelectCodeButton = (props) => {
-    return <>PROPIO</>
+    return <><span style={{fontStyle:"italic",fontSize:"0.9em"}}>Propio</span> <span style={{fontSize:"1.1em"}}><EditOutlined /></span></>
 }
 
 export default SelectCodeButton
