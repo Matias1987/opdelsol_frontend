c3dda67cdcb6545a4c9370f272441cb6d99229cd
bruno <ch.matias87@gmail.com>
2025-08-15 07:24:57 -0300

    varios

--- pages/v1/admin/cm/index.jsx
+++ pages/v1/admin/cm/index.jsx
@@ -1,6 +1,7 @@
 import Transferencia from "@/components/caja_master/transferencia";
 import LayoutAdmin from "@/components/layout/layout_admin";
-import { Button, Card, Col, Menu, Modal, Row, Table } from "antd";
+import { ArrowDownOutlined, ArrowUpOutlined, HomeOutlined, InfoOutlined, OrderedListOutlined } from "@ant-design/icons";
+import { Button, Card, Col, Menu, Modal, Row, Statistic, Table, Tabs } from "antd";
 import { useState } from "react";
 /**
  * CMIndex Page: Admin Control Management muestra totales de la caja master, y tareas posibles, como transferencia a fondos fijos,
@@ -30,29 +31,83 @@ const selectedStyle = {
   borderRadius: '6px',
 };*/
 
-const buttonStyle = { 
-  borderRadius: '6px',
-  padding: '8px 16px',
-  margin: '4px',
-  backgroundColor: '#f0f0f0',
-  border: '1px solid #d9d9d9',
-  boxShadow: '0 2px 8px rgba(0, 0, 0, 0.1)',
-  transition: 'background-color 0.3s',
-  color:"#0000ff"
-};
+  const buttonStyle = {
+    borderRadius: '6px',
+    padding: '8px 16px',
+    margin: '4px',
+    backgroundColor: '#f0f0f0',
+    border: '1px solid #d9d9d9',
+    boxShadow: '0 2px 8px rgba(0, 0, 0, 0.1)',
+    transition: 'background-color 0.3s',
+    color: "#0000ff"
+  };
 
   return (
     <>
-      <Card
-      title="Control de Cajas"
-      size="small"
-      extra={<>
-      
-      </>}
-      >
-        <Row>
-          <Col span={24}>
-           { /*<Menu
+    <Card title="Caja">
+      <Tabs
+        type="card"
+        defaultActiveKey="1"
+        items={
+          [
+            {
+              key: '1',
+              label: <><HomeOutlined/> Resumen</>,
+              children: <>
+                <Row gutter={16}>
+                  <Col span={12}>
+                    <Card variant="borderless">
+                      <Statistic
+                        title="Active"
+                        value={11.28}
+                        precision={2}
+                        valueStyle={{ color: '#3f8600' }}
+                        prefix={<ArrowUpOutlined />}
+                        suffix="%"
+                      />
+                    </Card>
+                  </Col>
+                  <Col span={12}>
+                    <Card variant="borderless">
+                      <Statistic
+                        title="Idle"
+                        value={9.3}
+                        precision={2}
+                        valueStyle={{ color: '#cf1322' }}
+                        prefix={<ArrowDownOutlined />}
+                        suffix="%"
+                      />
+                    </Card>
+                  </Col>
+                </Row>
+                <Row>
+            <Col span={24}>
+            <Table
+              size="small"
+              title={_ => <>Ultimas Operaciones
+              <Button size="small" style={buttonStyle} onClick={() => setIngresoPlataOpen(true)}>Nuevo Egreso</Button>
+              <Button size="small" style={buttonStyle} onClick={() => setIngresoPlataOpen(true)}>Nuevo Ingreso</Button>
+              </>}
+              dataSource={[]} columns={[]} pagination={false}
+            />
+          </Col>
+          </Row>
+              </>,
+            },
+            {
+              key: '2',
+              label: <><OrderedListOutlined /> Listado</>,
+              children: <>
+                <Card
+                  title="Control de Cajas"
+                  size="small"
+                  extra={<>
+
+                  </>}
+                >
+                  <Row>
+                    <Col span={24}>
+                      { /*<Menu
               style={{
                 background: "transparent",
                 border: "none",
@@ -76,33 +131,49 @@ const buttonStyle = {
                 { label: "Ingreso de Plata", key: "3" },
               ]}
             />*/}
-   
-          </Col>
-          <Col span={24}>
-        
-          </Col>
-        </Row>
-        <Row>
-          <Col span={24}>
-            <Table
-              size="small"
-              title={_ => <>Lista&nbsp;<Button size="small" style={buttonStyle} onClick={() => setIngresoPlataOpen(true)}>Nueva Caja</Button></>}
-              dataSource={[]} columns={[
-                {title:"Sucursal"},
-                {title:"Estado"},
-                {title:"Acciones"},
-              ]} pagination={false} 
-            />
-          </Col>
-          <Col span={24}>
-            <Table
-              size="small"
-              title={_ => <>Fondos Fijos&nbsp;<Button size="small" style={buttonStyle} onClick={() => setIngresoPlataOpen(true)}>Nueva Fondo Fijo</Button></>}
-              dataSource={[]} columns={[]} pagination={false}
-            />
-          </Col>
-        </Row>
-      </Card>
+
+                    </Col>
+                    <Col span={24}>
+
+                    </Col>
+                  </Row>
+                  <Row>
+                    <Col span={24}>
+                      <Table
+                        size="small"
+                        title={_ => <>Lista&nbsp;<Button size="small" style={buttonStyle} onClick={() => setIngresoPlataOpen(true)}>Nueva Caja</Button></>}
+                        dataSource={[]} columns={[
+                          { title: "Sucursal" },
+                          { title: "Estado" },
+                          { title: "Acciones" },
+                        ]} pagination={false}
+                      />
+                    </Col>
+
+                  </Row>
+                </Card>
+
+              </>,
+            },
+            {
+              label:<><InfoOutlined /> Fondos Fijos</>,
+              key: 3,
+              children: <>
+                <Row>
+                  <Col span={24}>
+                    <Table
+                      size="small"
+                      title={_ => <>Fondos Fijos&nbsp;<Button size="small" style={buttonStyle} onClick={() => setIngresoPlataOpen(true)}>Nuevo Fondo Fijo</Button></>}
+                      dataSource={[]} columns={[]} pagination={false}
+                    />
+                  </Col>
+                </Row>
+              </>
+            }
+          ]
+        } onChange={_ => { }} 
+      />
+    </Card>
       <Modal
         onCancel={() => setTransferenciaFondoFOpen(false)}
         open={transferenciaFondoFOpen}
