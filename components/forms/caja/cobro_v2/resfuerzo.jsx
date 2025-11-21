6d56aa7a8690a39ffe109568275737779006cb1f
bruno <ch.matias87@gmail.com>
2025-11-21 07:11:56 -0300

    varios

--- components/forms/caja/cobro_v2/resfuerzo.jsx
+++ components/forms/caja/cobro_v2/resfuerzo.jsx
@@ -1,6 +1,196 @@
 const Resfuerzo = props =>{
 
-    return <></>
+    return (
+        <>
+          <>
+            <h3>{typeof props.title === "undefined" ? "Cobro" : props.title}</h3>
+            <Row>
+              <Col span={24}>{cliente_detalle()}</Col>
+            </Row>
+            <Row>
+              <Col span={24}>{venta_detalle()}</Col>
+            </Row>
+            <Row>
+              <Col span={24}>
+                <ModoPagoV4
+                  idventa={
+                    typeof props.idventa === "undefined" ? -1 : props.idventa
+                  }
+                  mostrarSoloCtaCte={props.tipo != "ingreso"}
+                  totalsHidden={
+                    typeof props.totalsHidden === "undefined"
+                      ? true
+                      : props.totalsHidden
+                  }
+                  callback={onMPChange}
+                  total={
+                    dataVenta == null
+                      ? 0
+                      : parseFloat(dataVenta.subtotal) -
+                        parseFloat(descuento) -
+                        parseFloat(dataVenta.haber || 0)
+                  }
+                  ctacteHidden={
+                    typeof props.ctacteHidden !== undefined
+                      ? props.ctacteHidden
+                      : false
+                  }
+                  tarjetaHidden={
+                    typeof props.tarjetaHidden !== undefined
+                      ? props.tarjetaHidden
+                      : false
+                  }
+                  chequeHidden={
+                    typeof props.chequeHidden !== undefined
+                      ? props.chequeHidden
+                      : false
+                  }
+                  mutualHidden={
+                    typeof props.mutualHidden !== undefined
+                      ? props.mutualHidden
+                      : false
+                  }
+                />
+              </Col>
+            </Row>
+    
+            {estado_switch()}
+    
+            {props.tipo == "cuota" && mp != null ? (
+              <Button
+                type="primary"
+                onClick={onCobrarClick}
+                disabled={cobrarDisabled || mp.total < 1}
+              >
+                Cobrar
+              </Button>
+            ) : (
+              <></>
+            )}
+    
+            {dataVenta == null || mp == null ? (
+              <></>
+            ) : (
+              <>
+                <Row>
+                  <Col span={24}>
+                    <Divider />
+    
+                    {
+                      //entrega para ventas sin deuda
+                      parseFloat(dataVenta.subtotal) -
+                        parseFloat(descuento) -
+                        parseFloat(dataVenta.haber || 0) ==
+                        0 &&
+                      mp.total == 0 &&
+                      (entrega || props.tipo == "entrega") ? (
+                        <Button
+                          onClick={onCobrarClick}
+                          disabled={cobrarDisabled}
+                          danger
+                        >
+                          Entrega
+                        </Button>
+                      ) : (
+                        <></>
+                      )
+                    }
+                    {
+                      //resfuerzo con saldo 0 posterior
+                      mp.total != 0 &&
+                      (props.tipo == "resfuerzo" || props.tipo == "ingreso") ? (
+                        <Button
+                          onClick={onCobrarClick}
+                          disabled={cobrarDisabled}
+                          danger
+                        >
+                          Cobrar
+                        </Button>
+                      ) : (
+                        <></>
+                      )
+                    }
+                    {
+                      //intento de entrega pero con saldo distinto a 0
+                      props.tipo == "entrega" &&
+                      parseFloat(dataVenta.subtotal) -
+                        parseFloat(descuento) -
+                        parseFloat(dataVenta.haber || 0) -
+                        +mp.total !=
+                        0 &&
+                      mp.total != 0 ? (
+                        <Button
+                          onClick={onCobrarClick}
+                          disabled={cobrarDisabled}
+                          danger
+                        >
+                          Cobro Resfuerzo
+                        </Button>
+                      ) : (
+                        <></>
+                      )
+                    }
+                    {
+                      //intento de entrega pero con saldo distinto a 0
+                      props.tipo == "entrega" &&
+                      parseFloat(dataVenta.subtotal) -
+                        parseFloat(descuento) -
+                        parseFloat(dataVenta.haber || 0) !=
+                        0 &&
+                      parseFloat(dataVenta.subtotal) -
+                        parseFloat(descuento) -
+                        parseFloat(dataVenta.haber || 0) -
+                        +mp.total ==
+                        0 ? (
+                        <Button
+                          onClick={onCobrarClick}
+                          disabled={cobrarDisabled}
+                          danger
+                        >
+                          Entrega
+                        </Button>
+                      ) : (
+                        <></>
+                      )
+                    }
+                    {props.tipo == "ingreso" && !entrega ? (
+                      <>
+                        &nbsp;
+                        <Button
+                          disabled={mp.total > 0 || cobrarDisabled}
+                          type="primary"
+                          onClick={enviarADepositoClick}
+                        >
+                          Enviar a dep&oacute;sito{" "}
+                        </Button>
+                      </>
+                    ) : (
+                      <></>
+                    )}
+                  </Col>
+                </Row>
+              </>
+            )}
+          </>
+          {/* informe x */}
+          <Modal
+            maskClosable={false}
+            width={"800px"}
+            title={"Recibo X"}
+            open={informeOpen}
+            onOk={() => {
+              setInformeOpen(false);
+            }}
+            onCancel={onCloseInformePopup}
+            destroyOnClose={true}
+            footer={null}
+          >
+            <PrinterWrapper>
+              <InformeX idcobro={idCobro} />
+            </PrinterWrapper>
+          </Modal>
+        </>
+      );
 }
 
 export default Resfuerzo
--- components/forms/ventas/SearchStockVentas.jsx
+++ components/forms/ventas/SearchStockVentas.jsx
@@ -1,4 +1,4 @@
-import { Button, Table, Input, Row, Col, Flex, Space } from "antd";
+import { Button, Table, Input, Row, Col, Flex, Space, Switch } from "antd";
 import { useEffect, useState } from "react";
 import { post } from "@/src/urls";
 import globals from "@/src/globals";
@@ -13,126 +13,130 @@ import { regex_get_id_if_match } from "@/src/helpers/barcode_helper";
  * @param hideExtOpt
  */
 const SearchStockVentas = (props) => {
-    const {hideExtOpt} = props;
+    const { hideExtOpt } = props;
     const id_sucursal = globals.obtenerSucursal();
     const search_url = post.obtener_stock_ventas;
     const [dataSource, setDataSource] = useState([])
     const [loading, setLoading] = useState(false)
     const [filtros, setFiltros] = useState({
-        mainval: "", esf:"",cil:"",add:""
+        mainval: "", esf: "", cil: "", add: ""
     })
-    
-    const onSearch = (e) => {
-        const id = regex_get_id_if_match(e?.target?.value?.toUpperCase()||"")
-          
-        var fil = id>0 ? "" : filtros.mainval
-        if(id>0)
-        {
-            setFiltros(f=>({...f,mainval:""}))
-        }
-        const _srchval = `${fil}${filtros.esf == "" ? "" : " ESF"+filtros.esf}${filtros.cil == "" ? "" : " CIL" + filtros.cil}${filtros.add == "" ? "" : " ADD"+filtros.add}`
-        setLoading(true)
-        
-        const filters= {filtroCod: _srchval, idSucursal: id_sucursal, filtroFamilias: typeof props.idfamilias === 'undefined' ? [] : props.idfamilias, idcodigo: id }
-        
-        post_method(search_url, filters,
-        (_response)=>{
-            var response = typeof props.onParseResponse !== 'undefined' ? props.onParseResponse(_response) : _response;
 
-            setDataSource(
-                response.data.map(
-                    (row) => ({
-                        key: row.idcodigo,
-                        codigo: row.codigo,
-                        descripcion: row.descripcion,
-                        idcodigo: row.idcodigo,
-                        })
-                )
-            )
-            setLoading(false)
+    const onSearch = (e) => {
+        const id = regex_get_id_if_match(e?.target?.value?.toUpperCase() || "")
+
+        var fil = id > 0 ? "" : filtros.mainval
+        if (id > 0) {
+            setFiltros(f => ({ ...f, mainval: "" }))
         }
+        const _srchval = `${fil}${filtros.esf == "" ? "" : " ESF" + filtros.esf}${filtros.cil == "" ? "" : " CIL" + filtros.cil}${filtros.add == "" ? "" : " ADD" + filtros.add}`
+        setLoading(true)
+
+        const filters = { filtroCod: _srchval, idSucursal: id_sucursal, filtroFamilias: typeof props.idfamilias === 'undefined' ? [] : props.idfamilias, idcodigo: id }
+
+        post_method(search_url, filters,
+            (_response) => {
+                var response = typeof props.onParseResponse !== 'undefined' ? props.onParseResponse(_response) : _response;
+
+                setDataSource(
+                    response.data.map(
+                        (row) => ({
+                            key: row.idcodigo,
+                            codigo: row.codigo,
+                            descripcion: row.descripcion,
+                            idcodigo: row.idcodigo,
+                        })
+                    )
+                )
+                setLoading(false)
+            }
         )
     }
 
 
     return (
         <>
-        <Row>
-            <Col span={24} style={{padding:"4px"}}>
-                <Input 
-                allowClear
-                size="large"
-                value={filtros.mainval}
-                placeholder="Ingrese valor de búsqueda"
-                onChange={(e)=>{
-                    setFiltros(
-                        _f=>({..._f,mainval:e.target.value})
-                        )
-                    }
-                    }
+            <Row>
+                <Col span={24} style={{ padding: "4px" }}>
+                    <Input
+                        suffix={<>
+                        <Switch 
+                            checkedChildren="Nro."
+                            unCheckedChildren="Descripción" 
+                        /></>}
+                        allowClear
+                        size="large"
+                        value={filtros.mainval}
+                        placeholder="Ingrese valor de búsqueda"
+                        onChange={(e) => {
+                            setFiltros(
+                                _f => ({ ..._f, mainval: e.target.value })
+                            )
+                        }
+                        }
 
-                onKeyUp={(e)=>{
-                    if (e.key === 'Enter') {
-                        
-                        onSearch(e)
-                    }
-                }}
-                    
-                />
-            </Col>
-        </Row>
-        
-            { ((hideExtOpt||"0") ==="1") ? <></> : <Space direction="vertical" size="middle">
-            <Space.Compact size="middle">
-                <Input step={.25} min={-20} max={20} prefix="Esf.:" value={filtros.esf} onKeyUp={(e)=>{
-                    if (e.key === 'Enter') {
-                        onSearch(e)
-                    }
-                }} onChange={(e)=>{setFiltros(_f=>({..._f,esf:e.target.value}))}}/>
-            
-                <Input prefix="Cil.:" step={.25} min={-20} max={20} value={filtros.cil} onKeyUp={(e)=>{
-                    if (e.key === 'Enter') {
-                        onSearch(e)
-                    }
-                }}onChange={(e)=>{setFiltros(_f=>({..._f,cil:e.target.value}))}}/>
-            
-                <Input prefix="Add.:" step={.25} min={-20} max={20} value={filtros.add} onKeyUp={(e)=>{
-                    if (e.key === 'Enter') {
-                        onSearch(e)
-                    }
-                }}onChange={(e)=>{setFiltros(_f=>({..._f,add:e.target.value}))}}/>
-            <Button danger type="text" onClick={(e)=>{setFiltros(_f=>({esf:"", cil:"",add:""}))}}><CloseOutlined /></Button>
-            </Space.Compact>
+                        onKeyUp={(e) => {
+                            if (e.key === 'Enter') {
+
+                                onSearch(e)
+                            }
+                        }}
+
+                    />
+                </Col>
+            </Row>
+
+            {(( "1") === "1") ? <></> : <Space direction="vertical" size="middle">
+                <Space.Compact size="middle">
+                    <Input step={.25} min={-20} max={20} prefix="Esf.:" value={filtros.esf} onKeyUp={(e) => {
+                        if (e.key === 'Enter') {
+                            onSearch(e)
+                        }
+                    }} onChange={(e) => { setFiltros(_f => ({ ..._f, esf: e.target.value })) }} />
+
+                    <Input prefix="Cil.:" step={.25} min={-20} max={20} value={filtros.cil} onKeyUp={(e) => {
+                        if (e.key === 'Enter') {
+                            onSearch(e)
+                        }
+                    }} onChange={(e) => { setFiltros(_f => ({ ..._f, cil: e.target.value })) }} />
+
+                    <Input prefix="Add.:" step={.25} min={-20} max={20} value={filtros.add} onKeyUp={(e) => {
+                        if (e.key === 'Enter') {
+                            onSearch(e)
+                        }
+                    }} onChange={(e) => { setFiltros(_f => ({ ..._f, add: e.target.value })) }} />
+                    <Button danger type="text" onClick={(e) => { setFiltros(_f => ({ esf: "", cil: "", add: "" })) }}><CloseOutlined /></Button>
+                </Space.Compact>
             </Space>
             }
 
-        <Row style={{padding:"1em"}}>
-            <Col span={24}>
-                <Button type="primary" size="small" style={{borderRadius:"16px"}} onClick={onSearch} block><SearchOutlined /> Buscar</Button>
-            </Col>
-        </Row>
-        <Row>
-            <Col span={24}>
-                <Table 
-                    scroll={{y:"300px"}}
-                    rowClassName={(record, index) => index % 2 === 0 ? 'table-row-light' :  'table-row-dark'}
-                    pagination={false}
-                    loading={loading}
-                    dataSource={dataSource} 
-                    columns={
-                        [
-                            {title:"Codigo", dataIndex: "codigo",},
-                            {title:"Descripcion", dataIndex: "descripcion",},
-                            {
-                                title:"", 
-                                dataIndex: "idcodigo",
-                                render: 
-                                    (_,{idcodigo})=><Button style={{color:"blue"}} onClick={()=>{props.callback(idcodigo)}}><CheckCircleFilled /> Seleccionar</Button>
-                            },
-                        ]
-                    }
-                />
-            </Col>
+            <Row style={{ padding: "1em" }}>
+                <Col span={24}>
+                    <Button type="primary" size="small" style={{ borderRadius: "16px" }} onClick={onSearch} block><SearchOutlined /> Buscar</Button>
+                </Col>
+            </Row>
+            <Row>
+                <Col span={24}>
+                    <Table
+                        scroll={{ y: "300px" }}
+                        rowClassName={(record, index) => index % 2 === 0 ? 'table-row-light' : 'table-row-dark'}
+                        pagination={false}
+                        loading={loading}
+                        dataSource={dataSource}
+                        columns={
+                            [
+                                { title: "Codigo", dataIndex: "codigo", },
+                                { title: "Descripcion", dataIndex: "descripcion", },
+                                {
+                                    title: "",
+                                    dataIndex: "idcodigo",
+                                    render:
+                                        (_, { idcodigo }) => <Button style={{ color: "blue" }} onClick={() => { props.callback(idcodigo) }}><CheckCircleFilled /> Seleccionar</Button>
+                                },
+                            ]
+                        }
+                    />
+                </Col>
             </Row>
         </>
     )
